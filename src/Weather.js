3850f66e53b628ed1030a63c7eb74e18c8297957
alja1313 <olgaslahota@gmail.com>
2024-10-23 17:28:34 +0200

    Change line 10 and 12 error

--- src/Weather.js
+++ src/Weather.js
@@ -7,9 +7,8 @@ export default function Weather(props) {
       `The weather in ${response.data.name} is ${response.data.main.temp}°C`
     );
   }
-  let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
-  let apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric";
+  let apiKey = `50fa4024e3b1d5eac2f51ab18a47e997`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
   return <h2>Hello from Weather App! ☁🌡</h2>;
